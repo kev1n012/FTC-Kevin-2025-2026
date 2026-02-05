f92af483d9cd5ce686ea800a76f965059ba9b429
kev1n012 <94055@roc-teraa.nl>
2026-02-05 22:19:35 +0100

    Improved on Autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Dodge_Blue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Dodge_Blue.java
@@ -19,6 +19,9 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import java.util.List;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+
 @Autonomous(name = "Dodge_Blue")
 public class Dodge_Blue extends LinearOpMode {
 
@@ -36,46 +39,75 @@ public class Dodge_Blue extends LinearOpMode {
     private final double CLOSE_SERVO_POS = 0.0;
     private final double AUTO_AIM_GAIN = 0.015;
 
+    // Shooting function
+    public boolean SERVO_STATE = false;
+    public int TOTAL_BALLS = 3;
+    public  int CURRENT_BALLS = 0;
+    public int RPM_BALL_COUNTER = 3000;
+
+    public boolean HAS_COUNTED = false;
+
     public double cm(double centimeters) { return centimeters / 2.54; }
 
     private double calculateRPM(DcMotorEx motor) {
         return (motor.getVelocity() / TICKS_PER_REVOLUTION) * 60.0;
     }
 
-    public Action Shoot(){
-        return telemetry -> {
+    public Action Shoot() {
+        return new Action() {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                double rightRPM = calculateRPM(flywheelR);
+                double leftRPM = calculateRPM(flywheelL);
 
-            int balls = 3;
-            int current_balls = 0;
+                flywheelL.setPower(SHOOTER_POWER);
+                flywheelR.setPower(SHOOTER_POWER);
+                intake.setPower(INTAKE_SHOOT_POWER);
+                helper_motor.setPower(1.0);
 
-            double rightFlywheelSpeed;
-            double leftFlywheelSpeed;
 
-            rightFlywheelSpeed = calculateRPM(flywheelR);
-            leftFlywheelSpeed = calculateRPM(flywheelL);
+                telemetry.addData("Current balls", CURRENT_BALLS);
 
-            flywheelL.setPower(SHOOTER_POWER);
-            flywheelR.setPower(SHOOTER_POWER);
+                telemetry.update();
+                if (rightRPM > DESIRED_FLYWHEEL_SPEED && leftRPM > DESIRED_FLYWHEEL_SPEED) {
+                    shooter_servo.setPosition(OPEN_SERVO_POS);
+                    SERVO_STATE = true;
 
-            if (rightFlywheelSpeed >= DESIRED_FLYWHEEL_SPEED && leftFlywheelSpeed >= DESIRED_FLYWHEEL_SPEED) {
-                shooter_servo.setPosition(OPEN_SERVO_POS);
-            } else if (rightFlywheelSpeed <= CLOSE_SERVO_SPEED && leftFlywheelSpeed <= CLOSE_SERVO_SPEED) {
-                shooter_servo.setPosition(CLOSE_SERVO_POS);
+                    if(CURRENT_BALLS == TOTAL_BALLS){
+                        return false;
+                    }
+
+                    if(rightRPM < RPM_BALL_COUNTER || leftRPM < RPM_BALL_COUNTER){{
+                        if(HAS_COUNTED){
+                            CURRENT_BALLS++;
+                        }
+                        if(rightRPM > RPM_BALL_COUNTER || leftRPM > RPM_BALL_COUNTER){
+                            return true;
+                        }
+                        HAS_COUNTED = false;
+
+                        if(CURRENT_BALLS == TOTAL_BALLS){
+                            shooter_servo.setPosition(CLOSE_SERVO_POS);
+                            return false;
+
+                        }
+                    }
+                    }
+                }
+                return true;
             }
-
-            if(rightFlywheelSpeed < 3000 || leftFlywheelSpeed < 3000){
-                ++current_balls;
-            }
-
-            if(current_balls == balls){
-                return false;
-            }
-            return true;
         };
     }
 
+
+
     @Override
     public void runOpMode() {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        TelemetryPacket packet = new TelemetryPacket();
+
+
+
         Pose2d initialPose = new Pose2d(0, 0, 0);
         MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
 
@@ -99,7 +131,7 @@ public class Dodge_Blue extends LinearOpMode {
 
         // Build the Path
         Action trajectory = drive.actionBuilder(initialPose)
-                .strafeToLinearHeading(new Vector2d(110, 0), Math.toRadians(0))
+                .strafeToLinearHeading(new Vector2d(-43, 0), Math.toRadians(0))
                 .stopAndAdd(Shoot())
                 .build();
 
